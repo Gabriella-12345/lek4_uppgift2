0acd4f9d2a69ac579751416b7233d6b21fc8402d
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-20 22:44:12 +0200

    om ditt svar innehåller det den söker = true

--- src/App.java
+++ src/App.java
@@ -12,10 +12,10 @@ public class App {
 
         String ord = läsare.nextLine(); //*Skapat en string som heter: ord  */
 
-        System.out.println("Längden på ditt namn är: " + ord.length()); //* räknar längden */ 
+        System.out.println("Längden på ditt namn är: " + ord.length() + " " + "ord"); //* räknar längden */ 
 
 
-        System.out.print("Välj ett valfritt namn från ditt namn och skriv den här:");
+        System.out.print("skriv in EFTERNAMN här:");
         String test = läsare.nextLine();
         System.out.println(test);
         
@@ -25,19 +25,25 @@ public class App {
 
         String namn_1 = data.substring(0,plats1);
 
-        int plats2 = data.indexOf(" ", plats1+1);
+        // int plats2 = data.indexOf(" ", plats1+1);
 
         String namn_2 = data.substring(plats1,data.length());
 
         System.out.println("förnamn: "+namn_1+" efternamn:"+namn_2 );
 
+        System.out.println("Heter du "+ namn_1 +" i efternamn ?" );
+        String boo = läsare.nextLine();
+
+        boolean svar = boo.contains(namn_1) == false;
+        boolean svar2 = boo.contains(namn_2) == true;
+        System.out.println(svar);
+
+
+
+
+        // int boo = "namn_2"; 
+        // System.out.println(boo.contains("namn_2"));
         
-
-
-
-
-
-        System.out.println("Är det sannt att ditt efternamn är" + namn_2);
 
     }
 
