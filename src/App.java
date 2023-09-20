e6a4ef2142ce9240438768b97c32dbcbf07f1137
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-20 08:10:43 +0200

    balalalala

--- src/App.java
+++ src/App.java
@@ -1,18 +1,28 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    Scanner text = new scanner(System.in);
+    Scanner läsare = new scanner(System.in);
+    String ord;
+
 //*Uppgift a */
 
         System.out.print("Var god och skriv en mening:");
-        String ord = text.nextline(); //*Skapat en string som heter: ord  */
+        String ord = läsare.nextline(); //*Skapat en string som heter: ord  */
         System.out.println("Längden på din menning är: " + ord.lenght()); //* räknar längden */ 
 
 //* uppgigt b*/
 
-        System.out.print("Välj ett valfritt ord i från din mening och skriv den här:");
+        System.out.print("Välj ett valfritt ord i från din tidiagre mening och skriv den här:");
         
+        System.out.println(ord.indexOf("")); 
 
-    
+        System.out.print("Ditt ord ligger på plats" +  + "i meningen")
+//* uppgift c */
 
+        Substring
+//*uppgift d */
+        boolean 
+
+//* uppgift e */
     }
+
 }
